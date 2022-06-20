b450d51fffcd9d255eddd0b728a28e7acaa60c0c
unknown <oksanarak90@gmail.com>
2022-06-20 12:04:09 +0300

    Add new file

--- Truthy_And_Falsy_Assignment.js
+++ Truthy_And_Falsy_Assignment.js
@@ -6,19 +6,19 @@
 
 // let username = '';
 // let defaultName;
- 
+
 // if (username) {
 //   defaultName = username;
 // } else {
 //   defaultName = 'Stranger';
 // }
- 
+
 // console.log(defaultName); // Prints: Stranger
 // If you combine your knowledge of logical operators you can use a short-hand for the code above. In a boolean condition, JavaScript assigns the truthy value to a variable if you use the || operator in your assignment:
 
 // let username = '';
 // let defaultName = username || 'Stranger';
- 
+
 // console.log(defaultName); // Prints: Stranger
 // Because || or statements check the left-hand condition first, the variable defaultName will be assigned the actual value of username if it is truthy, and it will be assigned the value of 'Stranger' if username is falsy. This concept is also referred to as short-circuit evaluation.
 
